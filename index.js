c12a98d1d46940265b88ecf0fde2cdd5ca2d0dcb
KlyscSilva <smatheussilva@hotmail.com>
2023-05-08 22:43:20 -0300

    upprojet

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(express.urlencoded({ extended: true}));
+
+
+// criando middleware para carregar o css
+app.use(express.static("src"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configuração do MySQL
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '101521ba',
+  database: 'silva'
+});
+
+
+
+// Conexão com o MySQL
+connection.connect((error) => {
+  if (error) {
+    console.log('Erro ao conectar com o MySQL:', error);
+  } else {
+    console.log('Conexão com o MySQL estabelecida com sucesso!');
+  }
+});
+
+
+app.get('/', (req,res) =>{
+    res.sendFile(__dirname + "/src/home/index.html"); 
+})
+
+app.post('/cadastro', (req, res) => {
+
+  const { name, email, password} = req.body;
+  
+  const sql = `INSERT INTO usuarios1 (name, email, password) VALUES ('${name}', '${email}', '${password}')`;
+ 
+
+  connection.query(sql, (error, result) => {
+    if (error) {
+      console.log('Erro ao cadastrar usuário:', error);
+      res.status(500).send('Erro ao cadastrar usuário');
+    } else {
+      console.log('Usuário cadastrado com sucesso!');
+
+      res.status(200).send('Usuário cadastrado com sucesso!');
+      
+    }
+  });
+}); 
+
+
+// rota de cadastro
+app.get('/cadastro', (req, res) => {
+  res.sendFile(__dirname + "/src/Create-account/index.html"); 
+});
+
+
+// criando rota para validar o envio de login do formulário que está no banco
+
+// Rota para lidar com o envio do formulário de login
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  // Consulta SQL para selecionar o usuário pelo nome de usuário
+  const sql = `SELECT * FROM silva.usuarios1 WHERE email = ? AND password = ?`;
+  connection.query(sql, [email, password], (error, results) => {
+    if (error) {
+      throw error;
+    }
+  
+    if (results.length > 0) {
+      const user = results[0];
+  
+      // Senha válida, redireciona o usuário para a página principal
+      res.sendFile(__dirname + "/src/Sobre/index.html");
+    } else {
+      // E-mail ou senha inválidos, mostra uma mensagem de erro na tela de login
+      res.send('E-mail ou senha inválidos!');
+    }
+  });
+});
+
+
+
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('✅Servidor iniciado na porta 3000!✅');
+});
---
+++ src/script.js
@@ -0,0 +1,212 @@
+const button = document.querySelector("button-register");
+const form = document.getElementsByClassName("input-form");
+const form2 = document.getElementsByClassName("form-login");
+
+
+// Criando efeito para o cadeado de mostra e ocultar senha
+const passwordInput = document.getElementById("password")
+const eyeSvg = document.getElementById("eyeSvg")
+
+
+
+function eyeClick(){
+
+let inputTypePassWord = passwordInput.type === "password"
+
+if(inputTypePassWord){
+
+showPassword()
+
+}else{
+  hidePassword()
+}
+
+// Criando funções para aplicar as regras
+
+function showPassword(){
+
+  passwordInput.setAttribute("type", "text")
+  eyeSvg.setAttribute("src","/assets/open.png")
+
+}
+
+function hidePassword(){
+
+  passwordInput.setAttribute("type","password")
+  eyeSvg.setAttribute("src","/assets/closedd.png")
+}
+
+}
+
+// Fim das regras das funções de habilitar e desabilitar view de senha
+
+//Script de pular pagina
+//function irNextPage() {
+//  document.querySelector(".button-left").addEventListener("click", function () {
+   // window.location.href = "..home/index.html";
+
+  
+
+  // Adicione a classe de animação à página atual
+
+ //document.querySelector("body").classList.add("transition-right");
+ // setTimeout(function () {
+    //window.location.href = "..home/index.html";
+  //}, 1000); // 1000ms = 1 segundo
+//});
+
+
+//}
+
+function irNextPage() {
+  document.querySelector("body").addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+
+  // Adicione a classe de animação à página atual
+  document.querySelector("body").classList.add("transition-right");
+
+  // Espere a animação terminar antes de redirecionar para a página 2
+  setTimeout(function () {
+    window.location.href = "index.html";
+  }, 1000); // 1000ms = 1 segundo
+}
+
+
+function registerPage() {
+  // Adicione a classe de animação à página atual
+  document.querySelector("body").classList.add("transition-left");
+  // Espere a animação terminar antes de redirecionar para a página 1
+  setTimeout(function () {
+    window.location.href = "../Create-account/index.html";
+  }, 1000); // 1000ms = 1 segundo
+}
+
+// Retornar para pagina principal // 
+function logout() {
+  document.querySelector("body").addEventListener("click", function () {
+    window.location.href = "../home/index.html";
+  });
+
+  
+  setTimeout(function () {
+    window.location.href = "../home/index.html";;
+  }, 1000); // 1000ms = 1 segundo
+}
+
+
+// Fim do Script Pular Pagina
+
+// Criando eventos com função de submit e criando as variaveis para serem inseridas na função para o processo de validação
+form.addEventListener("submit", function (event) {
+  
+  // validando tela de login
+
+  const emailLogin = form2.elements["emailLogin"];
+  const passwordLogin = form2.elements["passwordLogin"];
+
+  if (!emailLogin.checkValidity()) {
+    alert("Email Inválido");
+    event.preventDefault();
+  }
+
+  if (!validarSenha(passwordLogin.value)) {
+    alert("Senha inválida");
+    event.preventDefault();
+  }
+
+  function validarSenha(passwordLogin) {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/;
+    return regex.test(passwordLogin);
+  }
+
+  function checkValidity(emailLogin) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(emailLogin);
+  }
+
+  // FIM DA VALIDACAO
+
+  // validando tela de cadastro
+  const nomeInput = form.elements["name"];
+  const emailInput = form.elements["email"];
+  const senhaInput = form.elements["password"];
+
+  if (!validarNome(nomeInput.value)) {
+    alert("Nome Inválido");
+    event.preventDefault();
+  }
+
+  if (!emailInput.checkValidity()) {
+    alert("Email Inválido");
+    event.preventDefault();
+  }
+
+  if (!validarSenha(senhaInput.value)) {
+    alert("Senha inválida");
+    event.preventDefault();
+  }
+});
+
+function validarNome(name) {
+  const regex = /^[a-zA-Z]+$/;
+  return regex.test(name);
+}
+
+function validarSenha(password) {
+  const regex = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/;
+  return regex.test(password);
+}
+
+function checkValidity(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+// FIM DA VALIDACAO
+
+// depois de realizar o cadastro com sucesso
+
+
+function cadastrarUsuario(event) {
+
+
+const formDataToJson = (formData) => {
+  const json = {};
+  for (const [key, value] of formData.entries()) {
+    json[key] = value;
+  }
+  return JSON.stringify(json)
+};
+
+  event.preventDefault();
+
+  const formulario = document.getElementById('meuFormulario');
+  const dados = new FormData(formulario);
+console.log(formulario);
+
+  fetch('/cadastro', {
+    method: 'POST',
+    body: formDataToJson(dados),
+    // headers:{
+    //   'Content-Type': 'multipart/form-data'
+    // }
+    headers:{
+       'Content-Type': 'application/json'
+    }
+
+  })
+  .then(response => response.text())
+  .then(data => {
+    // exibe a mensagem de alerta
+    const alerta = document.getElementById('alerta');
+    alerta.innerText = data;
+
+    // limpa os campos do formulário (opcional)
+    formulario.reset();
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+
